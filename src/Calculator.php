0b538fdf5e8cdaac60988d6b4d973b5e316ff601
pimonrat <themoo868@gmail.com>
2017-05-29 15:43:04 +0700

    Initial commit

---
+++ src/Calculator.php
@@ -0,0 +1,18 @@
+<?php
+namespace ellely\Calculator;
+class Calculator{
+    public function add($num1,$num2){
+        return $num1 + $num2;
+    }
+    public function del($num1,$num2){
+        return $num1 - $num2;
+    }
+    public function multiply($num1,$num2){
+        return $num1 * $num2;
+    }
+    public function divide($num1,$num2){
+        return $num1 / $num2;
+    }
+
+}
+?>
